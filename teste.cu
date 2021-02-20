98d162313958b24d131a524c4935fd5c3a91de6e
PADBarbosa <padbarbosa@gmail.com>
2021-02-20 19:30:57 +0000

    alteracoes na maneira de instalar e exemplo para testar cuda

---
+++ teste.cu
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Usado para testar se o sistema consegue executar código CUDA sem problemas
+
+
+__global__ void add( int a, int b, int *c ) {
+	*c = a + b;
+}
+
+int main( void ) {
+	int c;
+	int *dev_c;
+	cudaMalloc( (void**)&dev_c, sizeof(int) );
+	add<<<1,1>>>( 2, 11, dev_c );
+	cudaMemcpy( &c,	dev_c,	sizeof(int), cudaMemcpyDeviceToHost );
+	printf( "2 + 11 = %d\n", c );
+	cudaFree( dev_c );
+	return 0;
+}
