4a50defda81e7ef78754d1678ad36fc461ed1a8b
PADBarbosa <padbarbosa@gmail.com>
2021-02-22 16:54:32 +0000

    adicionado teste para CUDA com HPX

---
+++ HPX/testeCuda/CMakeFiles/solution9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solution9.dir/solution9_generated_solution9.cu.o"
+  "CMakeFiles/solution9.dir/solution9_intermediate_link.o"
+  "solution9"
+  "solution9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/solution9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
